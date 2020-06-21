1606ecb1a2373883a4b93457e74b4dfccf778203
phupfoem <phu.nguyenpfoem@hcmut.edu.vn>
2020-06-22 01:22:43 +0700

    FIXED TYPO: prioprity -> priority

--- src/queue.c
+++ src/queue.c
@@ -25,7 +25,7 @@ void enqueue(struct queue_t * q, struct pcb_t * proc)
 	while (index != 0) // node at root -> stop
 	  {
 		// the only maybe wrongly-placed node is placed right -> stop
-		if (q->proc[index]->prioprity <= q->proc[(index - 1) >> 1]->prioprity)
+		if (q->proc[index]->priority <= q->proc[(index - 1) >> 1]->priority)
 			break;
 		
 		// sieve the wrongly-placed node up
@@ -37,7 +37,7 @@ void enqueue(struct queue_t * q, struct pcb_t * proc)
 
 /*
 	UNDEFINED BEHAVIOR: if called when *q is empty
-	TODO: return a pcb whose prioprity is the highestin the queue [q] and remember to remove it from q
+	TODO: return a pcb whose priority is the highestin the queue [q] and remember to remove it from q
 */
 struct pcb_t * dequeue(struct queue_t * q)
 {
@@ -59,15 +59,15 @@ struct pcb_t * dequeue(struct queue_t * q)
 	  {
 		higherChild = (index << 1) + 1; // left child's index
 		
-		// if right child exists AND has higher prioprity than left child,
+		// if right child exists AND has higher priority than left child,
 		// then [higherChild] is right child's index
 		if (higherChild + 1 < q->size
-			&& q->proc[higherChild]->prioprity < q->proc[higherChild + 1]->prioprity)
+			&& q->proc[higherChild]->priority < q->proc[higherChild + 1]->priority)
 			++higherChild; // right child's index
 			
-		// if node [higherChild] has lower prioprity,
+		// if node [higherChild] has lower priority,
 		// then the only maybe wrongly-placed node is placed right -> stop
-		if (q->proc[higherChild]->prioprity < q->proc[index]->prioprity)
+		if (q->proc[higherChild]->priority < q->proc[index]->priority)
 			break;
 		
 		// swap node and node [higherChild]
